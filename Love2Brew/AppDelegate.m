ab5e702f68c9ee985057758c1b35f193599afa0a
Sean Reinhardt <seanreinhardtapps@outlook.com>
2015-01-29 20:53:37 -0800

    Registered notification, resized text on reminder page

--- Love2Brew/AppDelegate.m
+++ Love2Brew/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound categories:nil]];
+
+    
     return YES;
 }
 
